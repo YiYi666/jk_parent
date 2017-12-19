daef5e8a70359a44ddacced4088b6a09eb807b50
YiYi666 <1092855724@qq.com>
2017-12-19 23:29:13 +0800

    修改事务

--- jk_service/src/main/java/top.greathead.jk.service/impl/UserServiceImpl.java
+++ jk_service/src/main/java/top.greathead.jk.service/impl/UserServiceImpl.java
@@ -2,6 +2,7 @@ package top.greathead.jk.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import top.greathead.jk.dao.BaseDao;
 import top.greathead.jk.entity.User;
 import top.greathead.jk.service.UserService;
@@ -10,12 +11,13 @@ import javax.annotation.Resource;
 import java.util.List;
 
 @Service
+
 public class UserServiceImpl implements UserService {
 
     @Autowired
     private BaseDao baseDao;
 
-
+    @Transactional
     @Override
     public List<User> findlist() {
         List<User> list = baseDao.getListByHQL("from User");
